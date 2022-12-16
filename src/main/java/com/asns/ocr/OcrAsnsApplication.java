ee8fb4f374732da4c5d9e8a2047e30b3ab9b4ebc
sinsava <saurav.singh@thoughtworks.com>
2022-12-16 23:01:19 +0530

    First commit

---
+++ src/main/java/com/asns/ocr/OcrAsnsApplication.java
@@ -0,0 +1,13 @@
+package com.asns.ocr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrAsnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrAsnsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/asns/ocr/OcrAsnsApplicationTests.java
@@ -0,0 +1,10 @@
+package com.asns.ocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrAsnsApplicationTests {
+
+
+}
